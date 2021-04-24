0dc3db30e9fddbc679e5b890a4c280d35eb40e64
IotaNine <70635434+IotaNine@users.noreply.github.com>
2021-04-25 01:18:53 +0300

    Add files via upload

--- DZ1.cpp
+++ DZ1.cpp
@@ -1,97 +1,179 @@
-﻿//#include <cstdio>
-#include <fstream>
+﻿#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #define BLOCK_SIZE 16
 #define BLOCK_SHIFT 5
-// Направление сдвига влево
+//Shift direction: Left
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 struct Block
 {
     uint8_t Data[BLOCK_SIZE];
 };
 
-void TechFunc(std::string name)  // При необходимости дозаписывает в текст инормацию, чтобы кол-во байт было кратно BLOCK_SIZE
+unsigned long long TextSize(std::string& name)
 {
-    std::fstream file(name, std::ios::app);
+    std::fstream file(name);
     file.seekg(0, file.end);
-    long long bytes = file.tellg();
-    file.seekg(0, file.beg);
-    if (bytes % BLOCK_SIZE != 0)
-    {
-        int delta = BLOCK_SIZE - (bytes % BLOCK_SIZE);
-        std::string tmp = std::to_string(delta);
-        if (delta > 9) delta--;
-        for (int i = 1; i < delta; i++)
-        {
-            tmp += 'X';
-        }
-        file << tmp;
-    }
+    return file.tellg();
 }
 
-void ReadFromFile(std::string name, std::vector<Block>& blocks)
+unsigned int LastBlockSize(std::string& name)   
 {
-    TechFunc(name);
+    if (TextSize(name) % BLOCK_SIZE != 0)
+    {
+        return TextSize(name) % BLOCK_SIZE;
+    }
+    return BLOCK_SIZE;
+}
+
+void ReadFromFile(std::string& name, std::vector<Block>& blocks)
+{
     std::ifstream file(name, std::ios::binary);
     Block buffer;
-    while (!file.eof())
+    for(unsigned long long i = TextSize(name); i >= BLOCK_SIZE; i -= BLOCK_SIZE)
     {
         file.read(reinterpret_cast<char*>(&buffer), BLOCK_SIZE);
         blocks.push_back(buffer);
+        if (i - BLOCK_SIZE != 0 and i - BLOCK_SIZE < 16)
+        {
+            for (unsigned int j = 0; j < BLOCK_SIZE; j++)
+            {
+                file.read(reinterpret_cast<char*>(&buffer.Data[j]), 1);
+                if (j >= i - BLOCK_SIZE)
+                {
+                    buffer.Data[j] = '~';
+                }
+            }
+            blocks.push_back(buffer);
+        }
     }
-    blocks.pop_back();
 }
 
-void WriteToFile(std::string name, std::vector<Block>& blocks)
+void WriteToFile(std::string& name, std::vector<Block>& blocks)
 {
     std::ofstream file(name, std::ios::binary);
-    for (Block& buffer : blocks)
+    for(unsigned int i = 0; i < blocks.size(); i++)
     {
-        file.write(reinterpret_cast<char*>(&buffer), BLOCK_SIZE);
+        if (i == blocks.size() - 1)
+        {
+            for (unsigned int j = 0; j < BLOCK_SIZE; j++)
+            {
+                if (blocks[i].Data[j] != '~')
+                {
+                    file.write(reinterpret_cast<char*>(&blocks[i].Data[j]), 1);
+                }
+            }
+        }
+        else
+        {
+            file.write(reinterpret_cast<char*>(&blocks[i]), BLOCK_SIZE);
+        }
     }
 }
 
-void Gamming(std::vector<Block>& blocks, int& seed, bool dir)
+void Gamming(std::vector<Block>& blocks, int& seed)
 {
     for (Block& bl : blocks)
     {
         srand(seed);
-        for (int i = 0; i < BLOCK_SIZE; i++)
+        for (unsigned int i = 0; i < BLOCK_SIZE; i++)  
         {
             bl.Data[i] = bl.Data[i] ^ rand();
         }
-        if (dir)
-        {
-            seed++;
-        }
-        else
-        {
-            seed--;
-        }
+        seed++;
     }
 }
 
 void Shifting(std::vector<Block>& blocks, bool dir)
 {
-
+    if (dir == 1)
+    {
+        for (Block& bl : blocks)
+        {
+            for (unsigned int i = 0; i < BLOCK_SIZE; i++)
+            {
+                bl.Data[i] = bl.Data[i] << BLOCK_SHIFT | bl.Data[i] >> (8 - BLOCK_SHIFT);
+            }
+        }
+    }
+    else if (dir == 0)
+    {
+        for (Block& bl : blocks)
+        {
+            for (unsigned int i = 0; i < BLOCK_SIZE; i++)
+            {
+                bl.Data[i] = bl.Data[i] >> BLOCK_SHIFT | bl.Data[i] << (8 - BLOCK_SHIFT);
+            }
+        }
+    }
 }
 
-//class TextObj
-//{
-//public:
-//    std::vector<Block> Blocks;
-//    uint8_t Gamma[BLOCK_SIZE];
-//    TextObj();
-//    uint8_t& operator^(std::vector<Block>& blocks);
-//};
+void Encode(std::string& from, std::string& out, int& seed)
+{
+    std::vector<Block> Blocks;
+    ReadFromFile(from, Blocks);
+    Gamming(Blocks, seed);
+    Shifting(Blocks, 1);
+    WriteToFile(out, Blocks);
+}
+
+void Decode(std::string& from, std::string& out, int& seed)
+{
+    std::vector<Block> Blocks;
+    ReadFromFile(from, Blocks);
+    Shifting(Blocks, 0);
+    Gamming(Blocks, seed);
+    WriteToFile(out, Blocks);
+}
 
 int main()
 {
-    std::vector<Block> Blocks;
-    ReadFromFile("Original", Blocks);
-    WriteToFile("Changed", Blocks);
-    std::cout << "Hello World!\n";
+    unsigned int option = 0;
+    int key;
+    std::string First;
+    std::string Second;
+    while (option != 3)
+    {
+        cout << "Select option" << endl;
+        cout << "1. Encode" << endl << "2. Decode" << endl << "3. Exit" << endl;
+        cout << "Option: ";
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            cout << "File for encoding: ";
+            cin >> First;
+            cout << "File for recording: ";
+            cin >> Second;
+            cout << "Enter key: ";
+            cin >> key;
+            cout << "DONT'T FORGET THE KEY" << endl;
+            Encode(First, Second, key);
+            break;
+        case 2:
+            cout << "File for dencoding: ";
+            cin >> First;
+            cout << "File for recording: ";
+            cin >> Second;
+            cout << "Enter key: ";
+            cin >> key;
+            Decode(First, Second, key);
+        case 3:
+            break;
+        default:
+            cout << "There isn't such option" << endl;
+            cout << "Select option" << endl;
+            cout << "1. Encode" << endl << "2. Decode" << endl << "3. Exit" << endl;
+            cout << "Option: ";
+            cin >> option;
+            break;
+        }
+    }
+    cout << "SHUTDOWN";
 }
